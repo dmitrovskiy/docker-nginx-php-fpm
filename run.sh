1063debe19ec7fefb1857aa5703f87790ed8abaa
dmitrovskiy <vladimir@tep.io>
2016-08-31 21:00:07 +0300

    Feat: add nginx vars

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #nginx config
-NGINX_WORKER_PROCESES=${NGINX_WORKER_PROCESES:-auto}
+NGINX_WORKER_PROCESSES=${NGINX_WORKER_PROCESSES:-auto}
 NGINX_EVENTS_WORKER_CONNECTIONS=${NGINX_WORKER_CONNECTIONS:-1024}
 NGINX_EVENTS_USE=${NGINX_EVENTS_USE:-epoll}
 NGINX_EVENTS_MULTI_ACCEPT=${NGINX_EVENTS_MULTI_ACCEPT:-on}
@@ -16,11 +16,6 @@ NGINX_GZIP=${NGINX_GZIP:-on}
 NGINX_GZIP_STATIC=${NGINX_GZIP_STATIC:-on}
 NGINX_GZIP_COMP_LEVEL=${NGINX_GZIP_COMP_LEVEL:-5}
 NGINX_GZIP_MIN_LENGTH=${NGINX_GZIP_MIN_LENGTH:-1024}
-NGINX_GZIP_HTTP_VERSION=${NGINX_GZIP_HTTP_VERSION:-1.1}
-NGINX_GZIP_PROXIED=${NGINX_GZIP_PROXIED:-any}
-NGINX_GZIP_VARY=${NGINX_GZIP_VARY:-on}
-NGINX_GZIP_TYPES=${NGINX_GZIP_TYPES:-text/plain text/css application/json application/x-javascript text/xml application/xml application/xml+rss text/javascript application/javascript text/x-js}
-NGINX_GZIP_DISABLE=${NGINX_GZIP_DISABLE:-"msie6"}
 
 # fastcgi
 NGINX_FASTCGI_BUFFER_SIZE=${NGINX_FASTCGI_BUFFER_SIZE:-128k}
@@ -41,6 +36,25 @@ PHP_OPCACHE_MEMORY_CONSUMPTION=${PHP_OPCACHE_MEMORY_CONSUMPTION:-128}
 PHP_OPCACHE_MAX_ACCELERATED_FILES=${PHP_OPCACHE_MAX_ACCELERATED_FILES:-10000}
 PHP_OPCACHE_MAX_WASTED_PERCENTAGE=${PHP_OPCACHE_MAX_WASTED_PERCENTAGE:-10}
 PHP_OPCACHE_VALIDATE_TIMESTAMPS=${PHP_OPCACHE_VALIDATE_TIMESTAMPS:-0}
+
+sed -i "s|worker_processes.*;|worker_processes ${NGINX_WORKER_PROCESSES};|i" /etc/nginx/nginx.conf
+sed -i "s|worker_connections.*;|worker_connections ${NGINX_EVENTS_WORKER_CONNECTIONS};|i" /etc/nginx/nginx.conf
+sed -i "s|use\s.*;|use ${NGINX_EVENTS_USE};|i" /etc/nginx/nginx.conf
+sed -i "s|multi_accept.*;|multi_accept ${NGINX_EVENTS_MULTI_ACCEPT};|i" /etc/nginx/nginx.conf
+sed -i "s|sendfile.*;|sendfile ${NGINX_SENDFILE};|i" /etc/nginx/nginx.conf
+sed -i "s|tcp_nopush.*;|tcp_nopush ${NGINX_TCP_NOPUSH};|i" /etc/nginx/nginx.conf
+sed -i "s|tcp_nodelay.*;|tcp_nodelay ${NGINX_TCP_NODELAY};|i" /etc/nginx/nginx.conf
+sed -i "s|server_tokens.*;|server_tokens ${NGINX_SERVER_TOKENS};|i" /etc/nginx/nginx.conf
+sed -i "s|types_hash_max_size.*;|types_hash_max_size ${NGINX_TYPES_HASH_MAX_SIZE};|i" /etc/nginx/nginx.conf
+sed -i "s|gzip\s.*;|gzip ${NGINX_GZIP};|i" /etc/nginx/nginx.conf
+sed -i "s|gzip_static.*;|gzip_static ${NGINX_GZIP_STATIC};|i" /etc/nginx/nginx.conf
+sed -i "s|gzip_comp_level.*;|gzip_comp_level ${NGINX_GZIP_COMP_LEVEL};|i" /etc/nginx/nginx.conf
+sed -i "s|gzip_min_length.*;|gzip_min_length ${NGINX_GZIP_MIN_LENGTH};|i" /etc/nginx/nginx.conf
+
+sed -i "s|fastcgi_buffer_size.*;|fastcgi_buffer_size ${NGINX_FASTCGI_BUFFER_SIZE};|i" /etc/nginx/conf.d/default.conf
+sed -i "s|fastcgi_buffers.*;|fastcgi_buffers ${NGINX_FASTCGI_BUFFERS};|i" /etc/nginx/conf.d/default.conf
+sed -i "s|fastcgi_busy_buffers_size.*;|fastcgi_busy_buffers_size ${NGINX_FASTCGI_BUSY_BUFFERS_SIZE};|i" /etc/nginx/conf.d/default.conf
+sed -i "s|fastcgi_temp_file_write_size.*;|fastcgi_temp_file_write_size ${NGINX_FASTCGI_TEMP_FILE_WRITE_SIZE};|i" /etc/nginx/conf.d/default.conf
 
 sed -i "s|;*memory_limit\s*=\s*.*|memory_limit = ${PHP_MEMORY_LIMIT}|i" /etc/php5/php.ini
 sed -i "s|;*upload_max_filesize\s*=\s*.*|upload_max_filesize = ${PHP_MAX_UPLOAD}|i" /etc/php5/php.ini
